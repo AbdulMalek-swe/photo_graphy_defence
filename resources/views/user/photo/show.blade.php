ded3d17d41c1611181cebda6aabf2b655a98a0af
zanit-solution <zanltd.bd@gmail.com>
2025-01-11 11:28:31 +0600

    fix:order button

--- resources/views/user/photo/show.blade.php
+++ resources/views/user/photo/show.blade.php
@@ -95,8 +95,13 @@
                         <p class="card-text">{{ $p['description'] }}.</p>
                         
                         {{-- <a class="btn btn-success" href="{{route('photographer.hire',['title'=>$p['title'], 'price'=>$p['price'],'user_id'=>Auth::id(), 'photographer_id'=>$show->user_id])}}">Hire Me</a> --}}
-                        <a href={{ route('stripe.index') }}>wlcom vai</a>
-                        <button class="btn btn-success hire-btn" onclick="confirmAction()">Hire Me</button>
+                       
+                        <div class="text-center mt-4">
+                            <a href="{{ route('stripe.index') }}" class="btn btn-success hire-btn btn-lg px-4 py-2 text-white shadow-lg rounded-pill">
+                                Order
+                            </a>
+                        </div>
+                        
 
                         <script>
                             function confirmAction() {
